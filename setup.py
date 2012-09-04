987d522557912691f721a30e7f2f20b02815226e
Zach Snow <z@zachsnow.com>
2012-09-04 16:29:43 -0700

    Fix bad packaging and remove more HG stuff.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=__version__,
     author='Stuart Colville, modified by Zach Snow',
     author_email='z@zachsnow.com',
-    packages=find_packages('titlecase'),
+    packages=find_packages(),
     url='https://github.com/zachsnow/python-titlecase/',
     license='LICENSE.rst',
     description=r"""Python port of John Gruber's titlecase""",
